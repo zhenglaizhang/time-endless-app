0c3eec503e7f35f2c199f8b2d6b0c1a6d3e96d93
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-06-24 12:03:09 +0800

    init spring boot

---
+++ src/main/groovy/com/lianji/timeendlessapp/TimeEndlessAppApplication.groovy
@@ -0,0 +1,12 @@
+package com.lianji.timeendlessapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TimeEndlessAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TimeEndlessAppApplication, args
+	}
+}
